affb12838417d94651ee7022083087c0400aaed2
Mario <mario.gonzalez@hotmail.com.ar>
2023-04-29 17:57:42 -0300

    Ensayos perifericos DHT

--- Academico/D- Proyecto/SOFT/Soft - Prueba de periféricos/TST Grupo1_DHT11/src/main.cpp
+++ Academico/D- Proyecto/SOFT/Soft - Prueba de periféricos/TST Grupo1_DHT11/src/main.cpp
@@ -11,18 +11,25 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 #include <DHT.h>
-#define DHTPIN 4
+#define DHTPIN_1 0
+#define DHTPIN_2 2
+#define DHTPIN_3 15
+
 #define DHTTYPE DHT11
 
 
 const int rs = 14, en = 12, d4 = 33, d5 = 25, d6 = 26, d7 = 27;
 LiquidCrystal lcd(14,12,33,25,26,27);
-DHT dht(DHTPIN, DHTTYPE);
+DHT dht1(DHTPIN_1, DHTTYPE);
+DHT dht2(DHTPIN_2, DHTTYPE);
+DHT dht3(DHTPIN_3, DHTTYPE);
 
 
 void setup() {
   lcd.begin(20, 4);
-  dht.begin();
+  dht1.begin();
+  dht2.begin();
+  dht3.begin();
 }
  
 void loop() {
@@ -30,12 +37,24 @@ void loop() {
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Temperatura = ");
-  float t = dht.readTemperature();
-      if (isnan(t)) {
-      lcd.print("Failed to read from DHT sensor!");
-      return;
-    }
-  lcd.print(t);
+  float t1 = dht1.readTemperature();
+  float t2 = dht2.readTemperature();
+  float t3 = dht3.readTemperature();
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Temp-1 = ");
+  lcd.print(t1);
+  lcd.setCursor(0, 1);
+  lcd.print("Temp-2 = ");
+  lcd.print(t2);
+  lcd.setCursor(0, 2);
+  lcd.print("Temp-2 = ");
+  lcd.print(t3);
+
+
+
+
 
   delay(2000);
   lcd.clear();
